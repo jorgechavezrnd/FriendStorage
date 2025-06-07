9327a7c77a56cdf519d1b46d2beb2433491e1fc0
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-07 00:08:56 -0400

    Define the base structure of the first unit test for NavigationViewModel

--- FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
+++ FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -1,12 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FriendStorage.UI.ViewModel;
+using Xunit;
 
 namespace FriendStorage.UITests.ViewModel
 {
     public class NavigationViewModelTests
     {
+        [Fact]
+        public void ShouldLoadFriends()
+        {
+            var viewModel = new NavigationViewModel();
+
+            viewModel.Load();
+
+            // TODO: How to assert this?
+            // Assert.Equal(2, viewModel.Friends.Count);
+        }
     }
 }
