a6d38e7a503a18047b2b1a981966977f1601013d
jorgechavezrnd <jorgechavezrnd@gmail.com>
2025-06-08 20:53:23 -0400

    Configure AutoFac for FriendEditViewModel and FriendDataProvider

--- FriendStorage.UI/Startup/BootStrapper.cs
+++ FriendStorage.UI/Startup/BootStrapper.cs
@@ -3,6 +3,7 @@ using FriendStorage.DataAccess;
 using FriendStorage.UI.DataProvider;
 using FriendStorage.UI.View;
 using FriendStorage.UI.ViewModel;
+using Prism.Events;
 
 namespace FriendStorage.UI.Startup
 {
@@ -12,11 +13,20 @@ namespace FriendStorage.UI.Startup
         {
             var builder = new ContainerBuilder();
 
+            builder.RegisterType<EventAggregator>()
+                .As<IEventAggregator>().SingleInstance();
+
             builder.RegisterType<MainWindow>().AsSelf();
             builder.RegisterType<MainViewModel>().AsSelf();
 
+            builder.RegisterType<FriendEditViewModel>()
+                .As<IFriendEditViewModel>();
+
             builder.RegisterType<NavigationViewModel>()
                 .As<INavigationViewModel>();
+
+            builder.RegisterType<FriendDataProvider>()
+                .As<IFriendDataProvider>();
 
             builder.RegisterType<NavigationDataProvider>()
                 .As<INavigationDataProvider>();
